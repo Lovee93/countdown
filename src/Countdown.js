5796554824756ba13242f4a807e0351c6f23766c
Lovee93 <lovee.jain93@gmail.com>
2020-03-28 00:59:52 +1100

    Adding my COVID countdown project

---
+++ src/Countdown.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Countdown extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+      days: undefined,
+      hours: undefined,
+      minutes: undefined,
+      seconds: undefined
+    };
+  };
+
+   componentDidMount() {
+    this.interval = setInterval(() => {
+      const { deadline } = this.props;
+      const then = new Date(deadline).getTime();
+      const now = new Date().getTime();
+      const countdown = then-now;
+      const days = Math.floor(countdown / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((countdown%(1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+      const minutes = Math.floor((countdown % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((countdown % (1000 * 60)) / 1000);   
+
+      this.setState({ days, hours, minutes, seconds });
+    }, 1000);
+   }
+  
+  componentWillUnmount() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+
+  render() {
+    const { days, hours, minutes, seconds } = this.state;
+    // Mapping the date values to radius values
+    const daysRadius = mapNumber(days, 30, 0, 0, 360);
+    const hoursRadius = mapNumber(hours, 24, 0, 0, 360);
+    const minutesRadius = mapNumber(minutes, 60, 0, 0, 360);
+    const secondsRadius = mapNumber(seconds, 60, 0, 0, 360);
+    
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Time left to upskill: </h1>
+          <div className="wrapper"> 
+            <div className="items"> 
+                <SVGCircle radius={daysRadius} />
+                {days} 
+              <div>Days</div>
+            </div>
+            <div className="items"> 
+              <SVGCircle radius={hoursRadius} />
+              {hours} 
+              <div>Hours</div>
+            </div>
+            <div className="items"> 
+              <SVGCircle radius={minutesRadius} />
+              {minutes} 
+              <div>Minutes</div>
+            </div>
+            <div className="items"> 
+              <SVGCircle radius={secondsRadius} />
+              {seconds} 
+              <div>Seconds</div>
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+const SVGCircle = ({ radius }) => (
+    <svg className="countdown-svg">
+        <path
+            fill="none"
+            stroke="white"
+            stroke-width="4"
+            d={describeArc(50, 50, 48, 0, radius)}
+        />
+    </svg>
+);
+
+// From StackOverflow: https://stackoverflow.com/questions/5736398/how-to-calculate-the-svg-path-for-an-arc-of-a-circle
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    var angleInRadians = ((angleInDegrees - 90) * Math.PI) / 180.0;
+
+    return {
+        x: centerX + radius * Math.cos(angleInRadians),
+        y: centerY + radius * Math.sin(angleInRadians)
+    };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle) {
+    var start = polarToCartesian(x, y, radius, endAngle);
+    var end = polarToCartesian(x, y, radius, startAngle);
+
+    var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+    var d = [
+        'M',
+        start.x,
+        start.y,
+        'A',
+        radius,
+        radius,
+        0,
+        largeArcFlag,
+        0,
+        end.x,
+        end.y
+    ].join(' ');
+
+    return d;
+}
+
+// From StackOverflow: https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+function mapNumber(number, in_min, in_max, out_min, out_max) {
+    return (
+        ((number - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+    );
+}
+
+export default Countdown;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Countdown from './Countdown';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Countdown deadline="Apr 14, 2020 00:00:00"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
